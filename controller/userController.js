318bdd396d24e30c09d0025041ada02d18a3e8fc
Node <shlomi19880@gmail.com>
2026-04-13 22:10:21 +0300

    Add create profile functionality

--- controller/userController.js
+++ controller/userController.js
@@ -1,27 +1,25 @@
 const db = require('../dbConfig');
 
-//יצירת שם פרופיל
-function createProfile(req,res){
-    const {name, last, email, pass }= req.body;
+function createProfile(req, res) {
+    const { name, last, email, pass } = req.body;
 
-    if(!name || !last || email || pass){
-        return res.status(400).send("Missing requires fields");
+    if (!name || !last || !email || !pass) {
+        return res.status(400).send("Missing required fields");
     }
 
     const sql = `INSERT INTO users (firstname, lastname, password, email)
-    VALUES ('${name}', '${last}','${pass}' ,'${email}')`;
+    VALUES ('${name}', '${last}', '${pass}', '${email}')`;
 
-    bd.query(sql, (err, result)=>{
-        if(err){
+    db.query(sql, (err, result) => {
+        if (err) {
             console.log(err);
             return res.status(500).send("DB error");
         }
 
-        console.log("New user created");
         res.send("User created successfully");
-    })
+    });
 }
 
 module.exports = {
-    createUser
+    createProfile   // 🔥 אותו שם בדיוק
 };
